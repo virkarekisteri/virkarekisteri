ef187114dcae97ca206caddb867c0b0579f1eede
Ville Niemi <70947133+nniemi@users.noreply.github.com>
2024-10-10 23:22:55 +0300

    Add position

--- frontend/src/components/Modal/CreateVirkaModal.tsx
+++ frontend/src/components/Modal/CreateVirkaModal.tsx
@@ -1,9 +1,9 @@
-// CreateVirkaModal.tsx
-import { Box, TextField, Button } from '@mui/material';
+import { Box, TextField, Button, Grid2, IconButton, Typography } from '@mui/material';
 import Modal from '@mui/material/Modal';
 import React from 'react';
 import axios from 'axios';
 import { Form, Field } from 'react-final-form';
+import CloseIcon from '@mui/icons-material/Close';
 
 interface CreateVirkaModalProps {
   open: boolean;
@@ -11,6 +11,7 @@ interface CreateVirkaModalProps {
 }
 
 const CreateVirkaModal: React.FC<CreateVirkaModalProps> = ({ open, handleClose }) => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const onSubmit = async (values: any) => {
     try {
       await axios.post('https://your-api-url/posts', values);
@@ -23,46 +24,82 @@ const CreateVirkaModal: React.FC<CreateVirkaModalProps> = ({ open, handleClose }
   return (
     <Modal open={open} onClose={handleClose}>
       <Box
-        component="form"
         sx={{
           position: 'absolute',
           top: '50%',
           left: '50%',
           transform: 'translate(-50%, -50%)',
-          width: 400,
+          width: 1000,
           bgcolor: 'background.paper',
           border: '2px solid #000',
           boxShadow: 24,
-          p: 4,
+          p: 0,
+          borderRadius: 1,
         }}
       >
-        <Form
-          onSubmit={onSubmit}
-          render={({ handleSubmit, submitting, pristine }) => (
-            <form onSubmit={handleSubmit}>
-              <Field name="title">
-                {({ input }) => <TextField {...input} margin="normal" required fullWidth id="title" label="Title" />}
-              </Field>
-              <Field name="content">
-                {({ input }) => (
-                  <TextField
-                    {...input}
-                    margin="normal"
-                    required
-                    fullWidth
-                    multiline
-                    rows={4}
-                    id="content"
-                    label="Content"
-                  />
-                )}
-              </Field>
-              <Button type="submit" variant="contained" disabled={submitting || pristine}>
-                Save
-              </Button>
-            </form>
-          )}
-        />
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            p: 2,
+            borderBottom: '1px solid #ccc',
+            bgcolor: '#223B7C',
+            color: 'white',
+            borderTopLeftRadius: 1,
+            borderTopRightRadius: 1,
+          }}
+        >
+          <Typography variant="h6">Luo uusi virka</Typography>
+          <IconButton onClick={handleClose} sx={{ color: 'white' }}>
+            <CloseIcon />
+          </IconButton>
+        </Box>
+        <Box component="form" sx={{ p: 4 }}>
+          <Form
+            onSubmit={onSubmit}
+            render={({ handleSubmit, submitting, pristine }) => (
+              <form onSubmit={handleSubmit}>
+                <Grid2 container spacing={2}>
+                  <Grid2>
+                    <Field name="createdAt">
+                      {({ input }) => (
+                        <TextField
+                          {...input}
+                          margin="normal"
+                          required
+                          fullWidth
+                          id="createdAt"
+                          label="Created At"
+                          type="datetime-local"
+                        />
+                      )}
+                    </Field>
+                  </Grid2>
+                  <Grid2>
+                    <Field name="creationDecisionNumber">
+                      {({ input }) => (
+                        <TextField
+                          {...input}
+                          margin="normal"
+                          required
+                          fullWidth
+                          id="creationDecisionNumber"
+                          label="Päätösnumero"
+                        />
+                      )}
+                    </Field>
+                  </Grid2>
+                  <Grid2 sx={{ display: 'flex', justifyContent: 'flex-end', mt: 2 }}>
+                    <Button type="submit" variant="contained" disabled={submitting || pristine}>
+                      Save
+                    </Button>
+                  </Grid2>
+                </Grid2>
+              </form>
+            )}
+          />
+        </Box>
       </Box>
     </Modal>
   );
