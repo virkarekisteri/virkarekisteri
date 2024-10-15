f8112b1ab6fb6beb0ad4f57e4358259518160658
Joona Syrjämäki <65870202+joonasyr@users.noreply.github.com>
2024-10-15 17:20:02 +0300

    Vir 31 update position (#7)
    * Change Position model to use get;set instead of get;init
    
    * Create DTO for UpdatePosition
    
    * Create UpdatePosition function
    
    * Create entry in PositionRepository for UpdatePosition
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- backend/src/Models/Position.cs
+++ backend/src/Models/Position.cs
@@ -6,31 +6,31 @@ namespace Virkarekisteri.Models;
 [Table("Positions")]
 public class Position
 {
-    public Guid Id { get; init; }
+    public Guid Id { get; set; }
 
     [Required]
     [Column("LuontiPvm")]
-    public DateTime CreatedAt { get; init; }
+    public DateTime CreatedAt { get; set; }
 
     [Column("PaattymisPvm")]
-    public DateTime? EndedAt { get; init; }
+    public DateTime? EndedAt { get; set; }
 
     [Column("VakanssiKoko", TypeName = "decimal(3, 2)")]
-    public decimal? VacancySize { get; init; }
+    public decimal? VacancySize { get; set; }
 
     [Column("VakanssinTaytto", TypeName = "decimal(3, 2)")]
-    public decimal? VacancyFill { get; init; }
+    public decimal? VacancyFill { get; set; }
 
     [Required]
     [MaxLength(100)]
     [Column("LuontiPaatosNumero")]
-    public required string CreationDecisionNumber { get; init; }
+    public required string CreationDecisionNumber { get; set; }
 
     [MaxLength(100)]
     [Column("LopetusPaatosNumero")]
-    public string? EndingDecisionNumber { get; init; }
+    public string? EndingDecisionNumber { get; set; }
 
     [Required]
     [Column("Laji")]
-    public int Type { get; init; }
+    public int Type { get; set; }
 }
---
+++ backend/src/Models/UpdatePositionDto.cs
@@ -0,0 +1,8 @@
+namespace Virkarekisteri.Models
+{
+    public class UpdatePositionDto
+    {
+        public DateTime? EndedAt { get; set; }
+        public string? EndingDecisionNumber { get; set; }
+    }
+}
