1ce1223dab5e4162f04fe495c8174d00e944db31
Lassi Lappalainen <lassi.lappalainen@tuni.fi>
2024-10-25 14:04:43 +0300

    Fix move validation after deserialization

--- backend/src/Functions/Positions/CreatePosition.cs
+++ backend/src/Functions/Positions/CreatePosition.cs
@@ -27,14 +27,14 @@ public class CreatePosition(ILogger<CreatePosition> logger, PositionRepository p
 
         var (error, requestPosition) = await TryDeserializeRequestBody<Position>(req);
 
+        if (error is not null)
+            return error;
+
         // Check that the fill percentage is not greater than the vacancy percentage
         if (requestPosition.VacancyFill > requestPosition.VacancySize)
         {
             return new BadRequestObjectResult("The fill % cannot be greater than the total vacancy %");
         }
-
-        if (error is not null)
-            return error;
 
         var createdPosition = await positionRepository.CreatePosition(requestPosition);
         return new OkObjectResult(createdPosition);
