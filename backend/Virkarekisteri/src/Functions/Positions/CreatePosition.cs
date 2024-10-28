f3dcffa692c812e1b8dc2e97ed51733620462322
Otto <36742501+0x464e@users.noreply.github.com>
2024-10-28 21:54:58 +0200

    VIR 111 Setting up backend tests (#18)
    * Restructure backend to support unit testing
    
    * Bugfix GetPosition should return not found when position is not found
    
    * Setup FluentAssertions
    
    * Write GetPosition unit tests
    
    * Set up dependency injection for PositionNameRepository
    
    ---------
    
    Co-authored-by: Joona Syrjämäki <joona.syrjamaki@evitec.com>

--- backend/Virkarekisteri/src/Functions/Positions/CreatePosition.cs
+++ backend/Virkarekisteri/src/Functions/Positions/CreatePosition.cs
@@ -10,8 +10,8 @@ namespace Virkarekisteri.Functions.Positions;
 
 public class CreatePosition(
     ILogger<CreatePosition> logger,
-    PositionRepository positionRepository,
-    PositionNameRepository positionNameRepository
+    IPositionRepository positionRepository,
+    IPositionNameRepository positionNameRepository
 )
 {
     /// <summary>
@@ -41,15 +41,11 @@ public class CreatePosition(
                 return new BadRequestObjectResult("Either PositionNameId or a valid PositionName must be provided.");
             }
 
-            var positionNameId = await positionNameRepository.GetPositionNameIdByName(
-                requestPosition.PositionName.Name
-            );
-            if (positionNameId == null)
-            {
-                positionNameId = await positionNameRepository.CreatePositionName(requestPosition.PositionName.Name);
-            }
+            var positionNameId =
+                await positionNameRepository.GetPositionNameIdByName(requestPosition.PositionName.Name)
+                ?? await positionNameRepository.CreatePositionName(requestPosition.PositionName.Name);
 
-            requestPosition.PositionNameId = positionNameId.Value;
+            requestPosition.PositionNameId = positionNameId;
         }
 
         requestPosition.PositionName = null; // Nullify to avoid conflicts
