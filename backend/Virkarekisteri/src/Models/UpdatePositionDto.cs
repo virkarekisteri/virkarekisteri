ffe945d706bd238c9e76a839368676fd22ffe056
Joona Syrjämäki <65870202+joonasyr@users.noreply.github.com>
2024-10-28 22:13:54 +0200

    Vir 19 edit position (#15)
    * Add support for changing position name via updatePosition function
    
    * Update PositionRepositories to use interfaces
    
    ---------
    
    Co-authored-by: Joona Syrjämäki <joona.syrjamaki@evitec.com>
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- backend/Virkarekisteri/src/Models/UpdatePositionDto.cs
+++ backend/Virkarekisteri/src/Models/UpdatePositionDto.cs
@@ -4,6 +4,7 @@ namespace Virkarekisteri.Models
     {
         public DateTime? EndedAt { get; set; }
         public string? EndingDecisionNumber { get; set; }
+        public string? PositionName { get; set; }
         public decimal? VacancyFill { get; set; }
     }
 }
