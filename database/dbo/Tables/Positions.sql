1733595e60c32062bc94fa317ea157373774a5fc
Lassi Lappalainen <lassi.lappalainen@tuni.fi>
2024-10-30 01:46:18 +0200

    VIR-84: Placement location fix (#20)
    * Hotfix for database
    * Add update position

--- database/dbo/Tables/Positions.sql
+++ database/dbo/Tables/Positions.sql
@@ -8,11 +8,11 @@ CREATE TABLE [dbo].[Positions] (
     [LuontiPaatosNumero] NVARCHAR(50) NOT NULL,
     [LopetusPaatosNumero] NVARCHAR(50),
     [Laji] INT NOT NULL,
+    [Sijoituspaikka] NVARCHAR(50),
     [PositionNameId] UNIQUEIDENTIFIER CONSTRAINT [FK_Positions_PositionNames] FOREIGN KEY ([PositionNameId]) REFERENCES [dbo].[PositionNames] ([Id]),
     [Koulutustaso] NVARCHAR(255),
     [Tyokokemus] NVARCHAR(255),
-    [Lisatiedot] NVARCHAR(255),
-    [Sijoituspaikka] NVARCHAR(50)
+    [Lisatiedot] NVARCHAR(255)
 );
 
 GO
