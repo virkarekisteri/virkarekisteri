9825292629ba06322bc897714ff1ae36fe6c5c6f
Lassi Lappalainen <lassi.lappalainen@tuni.fi>
2024-11-08 02:20:21 +0200

    Add Vacancy number generatr so it can be requested

--- backend/Virkarekisteri/src/Program.cs
+++ backend/Virkarekisteri/src/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Virkarekisteri.Models;
 using Virkarekisteri.Repositories;
+using Virkarekisteri.Utils;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWebApplication()
@@ -16,6 +17,7 @@ var host = new HostBuilder()
             services.AddScoped<IPositionNameRepository, PositionNameRepository>();
             services.AddScoped<IOrganizationTreeRepository, OrganizationTreeRepository>();
             services.AddScoped<IPositionEmployeeRepository, PositionEmployeeRepository>();
+            services.AddScoped<VacancyNumberGenerator>();
             services.AddDbContext<VirkarekisteriDb>(options =>
                 options.UseSqlServer(
                     config.Configuration["SqlConnectionString"],
