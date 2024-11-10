05cffad2d066d4a41ab4c8a9d54e18d1cfed149f
Niina <170012548+niinakos@users.noreply.github.com>
2024-11-11 01:14:04 +0200

    First components for landing page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,26 @@
-import VirkarekisterContainer from 'components/VirkarekisteriContainer';
+import { Box, Button, Container, Typography } from "@mui/material";
 
 const App = () => {
   return (
     <div>
-      <VirkarekisterContainer />
+      <Container maxWidth="xs">
+        <Box
+        component="img"
+        sx={{
+          marginTop: 8,
+          height: "145px"
+        }}
+        alt="Logo"
+        src="/public/seinajoki-logo.jpg"
+        >
+        </Box>
+        <Typography component="h1" variant="h5" sx={{ textAlign: "center", marginTop: 2 }}>
+          Joku teksti
+        </Typography>
+        <Box textAlign='center' sx={{ marginTop: 4 }}>
+        <Button variant="contained" sx={{backgroundColor: "#223B7C"}}>Kirjaudu sisään</Button>
+        </Box>
+      </Container>
       {/* Other components and content will go here */}
     </div>
   );
