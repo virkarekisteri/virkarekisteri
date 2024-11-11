8c9f7e6b59420df3550ddf90d5389ed238f633fe
0x464e <36742501+0x464e@users.noreply.github.com>
2024-11-11 21:27:03 +0200

    Moving LandingPage to own component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,29 +1,5 @@
-import { Box, Button, Container, Typography } from "@mui/material";
+import LandingPage from './components/LandingPage';
 
-const App = () => {
-  return (
-    <div>
-      <Container maxWidth="xs">
-        <Box
-        component="img"
-        sx={{
-          marginTop: 8,
-          height: "145px"
-        }}
-        alt="Logo"
-        src="/public/seinajoki-logo.jpg"
-        >
-        </Box>
-        <Typography component="h1" variant="h5" sx={{ textAlign: "center", marginTop: 2 }}>
-          Joku teksti
-        </Typography>
-        <Box textAlign='center' sx={{ marginTop: 4 }}>
-        <Button variant="contained" sx={{backgroundColor: "#223B7C"}}>Kirjaudu sisään</Button>
-        </Box>
-      </Container>
-      {/* Other components and content will go here */}
-    </div>
-  );
-};
+const App = () => LandingPage();
 
 export default App;
