7ea5a89f7b12582571ffab411d5891a94f5ca1c9
Otto <36742501+0x464e@users.noreply.github.com>
2024-11-18 17:56:52 +0200

    VIR-48 authorize backend endpoints (#32)
    * Refactor attribute usage & enable hierarchical use of roles
    
    * Add role attributes to functions

--- backend/Virkarekisteri/src/Middleware/Attributes/RequiredRoleAttribute.cs
+++ backend/Virkarekisteri/src/Middleware/Attributes/RequiredRoleAttribute.cs
@@ -1,7 +1,25 @@
 ﻿namespace Virkarekisteri.Middleware.Attributes;
 
-[AttributeUsage(AttributeTargets.Method)]
-public class RequiredRoleAttribute(string role) : Attribute
+// Higher roles imply all lower roles
+// And in enums of course the order grows as we add members below
+// (remember enum members are constants of an undelying intregral type, in this case int, starting from 0)
+public enum RoleHierarchy
 {
-    public string Role { get; } = role;
+    Read,
+    Edit,
+    Admin,
 }
+
+public abstract class RequiredRoleAttribute(RoleHierarchy role) : Attribute
+{
+    public RoleHierarchy Role { get; } = role;
+}
+
+[AttributeUsage(AttributeTargets.Method)]
+public class RequiresReadRoleAttribute() : RequiredRoleAttribute(RoleHierarchy.Read);
+
+[AttributeUsage(AttributeTargets.Method)]
+public class RequiresEditRoleAttribute() : RequiredRoleAttribute(RoleHierarchy.Edit);
+
+[AttributeUsage(AttributeTargets.Method)]
+public class RequiresAdminRoleAttribute() : RequiredRoleAttribute(RoleHierarchy.Admin);
--- backend/Virkarekisteri/src/Middleware/RoleAuthorizationMiddleware.cs
+++ backend/Virkarekisteri/src/Middleware/RoleAuthorizationMiddleware.cs
@@ -14,6 +14,8 @@ public class RoleAuthorizationMiddleware : IFunctionsWorkerMiddleware
     public Task Invoke(FunctionContext context, FunctionExecutionDelegate next)
     {
         var httpContext = context.GetHttpContext();
+
+        // if we are not in a HTTP context, this middleware is not needed
         if (httpContext is null)
             return next(context);
 
@@ -27,18 +29,26 @@ public class RoleAuthorizationMiddleware : IFunctionsWorkerMiddleware
         var method = Assembly.GetExecutingAssembly().GetType(typeName)?.GetMethod(methodName);
 
         if (method is null)
-            return next(context);
+            throw new InvalidOperationException($"Unable to find method '{typeName}.{methodName}'");
 
         var roleRequiredAttribute = method.GetCustomAttribute<RequiredRoleAttribute>();
 
         if (roleRequiredAttribute is null)
-            return next(context);
+            throw new InvalidOperationException(
+                $"The method '{typeName}.{method.Name}' is missing a required role attribute"
+            );
 
+        // NOTE!
+        // We are not validating the JWT at all, because Azure is configured to do that the before code exectution
+        // ever even reaches this point. If you don't have Azure validating the token, you must do it here!
         var token = httpContext.Request.Headers.Authorization.ToString().Split(" ")[^1];
         var jwtToken = new JwtSecurityTokenHandler().ReadToken(token) as JwtSecurityToken;
-        var userRoles = jwtToken?.Claims.Where(c => c.Type == "roles").Select(c => c.Value);
+        var userRoles = jwtToken
+            ?.Claims.Where(c => c.Type == "roles")
+            .SelectMany(c => Enum.TryParse<RoleHierarchy>(c.Value, out var role) ? new[] { role } : []);
 
-        if (userRoles != null && userRoles.Contains(roleRequiredAttribute.Role))
+        // if any of the user's roles are higher up in the hierarchy than the required role, we're good
+        if (userRoles != null && userRoles.Any(userRole => userRole >= roleRequiredAttribute.Role))
             return next(context);
 
         httpContext.Response.StatusCode = StatusCodes.Status403Forbidden;
