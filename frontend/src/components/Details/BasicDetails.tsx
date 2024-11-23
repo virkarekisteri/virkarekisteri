c15dd268d28d26664d50bed27adb5cc96631d458
Niina <170012548+niinakos@users.noreply.github.com>
2024-11-23 06:20:45 +0200

    Additional position details inc. type, additional details and eligibility accordions

--- frontend/src/components/Details/BasicDetails.tsx
+++ frontend/src/components/Details/BasicDetails.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import { Accordion, AccordionDetails, AccordionSummary, CircularProgress, Typography } from '@mui/material';
+import { Accordion, AccordionDetails, AccordionSummary, alpha, Box, Typography } from '@mui/material';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import Grid2 from '@mui/material/Grid2';
 import RenderReadonlyTextField from './RenderReadonlyTextField';
-import { useAppSelector } from 'redux/hooks';
-import { selectPositionLoading } from 'redux/slices/position-slice';
 import type { Position } from 'models/Position';
 import { useTranslation } from 'react-i18next';
 
@@ -13,36 +11,130 @@ interface BasicDetailsProps {
 }
 
 const BasicDetails: React.FC<BasicDetailsProps> = ({ position }) => {
-  const loading = useAppSelector(selectPositionLoading);
   const { t } = useTranslation();
 
   return (
-    <Accordion defaultExpanded>
-      <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-        <Typography variant="h6">{t('position.details')}</Typography>
-      </AccordionSummary>
-      <AccordionDetails>
-        <Grid2 container spacing={2} sx={{ padding: 2, border: '1px solid #ccc', background: '#F5F5F5' }}>
-          {loading ? (
-            <CircularProgress />
-          ) : (
-            <>
-              <Grid2 size={{ xs: 12 }} container spacing={2} alignItems="center">
-                <Grid2 size={{ xs: 4 }}>
-                  <RenderReadonlyTextField label={t('table.type')} value={position.type.toString()} />
-                </Grid2>
-                <Grid2 size={{ xs: 8 }}>
-                  <RenderReadonlyTextField
-                    label={t('table.creation_decision_number')}
-                    value={position.creationDecisionNumber}
-                  />
-                </Grid2>
-              </Grid2>
-            </>
-          )}
+    <Box>
+      <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        p: 2,
+        height: 30,
+        borderBottom: '1px solid #ccc',
+        bgcolor: '#223B7C',
+        color: 'white',
+        borderTopLeftRadius: 1,
+        borderTopRightRadius: 1,
+      }}
+      >
+        <Typography>{t('position.details')}</Typography>
+      </Box>
+      <Box padding={3}>
+        <Grid2 container spacing={2} size={12}>
+          <Grid2 size={4} px={2}>
+            <Typography component={'div'} sx={{ color: '#7f7f7f' }}>
+              {t('table.vacancy_number')}
+            </Typography>
+            <RenderReadonlyTextField value={position.vacancyNumber} />
+          </Grid2>
+          <Grid2 size={4} px={2}>
+            <Typography component={'div'} sx={{ color: '#7f7f7f' }}>
+              {t('create_position.position_name')}
+            </Typography>
+            <RenderReadonlyTextField value={position.positionName.toString()} />
+          </Grid2>
+          <Grid2 size={4} px={2}>
+            <Typography component={'div'} sx={{ color: '#7f7f7f' }}>
+              {t('table.type')}
+            </Typography>
+            <RenderReadonlyTextField value={position.type.toString()} />
+          </Grid2>
+          <Grid2 size={4} px={2}>
+            <Typography component={'div'} sx={{ color: '#7f7f7f' }}>
+              {t('table.vacancy_status')}
+            </Typography>
+            <RenderReadonlyTextField value={position.vacancyStatus.toString()} />
+          </Grid2>
+          <Grid2 size={4} px={2}>
+            <Typography component={'div'} sx={{ color: '#7f7f7f' }}>
+              {t('create_position.vacancy_size')}
+            </Typography>
+            <RenderReadonlyTextField value={(position.vacancySize ? position.vacancySize * 100 : 0).toString()} />
+          </Grid2>
+          <Grid2 size={4} px={2}>
+            <Typography component={'div'} sx={{ color: '#7f7f7f' }}>
+              {t('basic_details.vacancy_fill')}
+            </Typography>
+            <RenderReadonlyTextField value={(position.vacancyFill ? position.vacancyFill * 100 : 0).toString()} />
+          </Grid2>
+          <Grid2 size={4} px={2}>
+            <Typography component={'div'} sx={{ color: '#7f7f7f' }}>
+              {t('table.placement_location')}
+            </Typography>
+            <RenderReadonlyTextField value={position.placementLocation} />
+          </Grid2>
+          <Grid2 size={4} px={2}>
+            <Typography component={'div'} sx={{ color: '#7f7f7f' }}>
+              {t('create_position.organization_tree')}
+            </Typography>
+            <RenderReadonlyTextField value={position.orgTreeId} />
+          </Grid2>
+          <Grid2 size={4} px={2}>
+            <Typography component={'div'} sx={{ color: '#7f7f7f' }}>
+              {t('create_position.pricing_id')}
+            </Typography>
+            <RenderReadonlyTextField value={position.pricingId?.toString()} />
+          </Grid2>
         </Grid2>
-      </AccordionDetails>
-    </Accordion>
+
+        <Accordion sx={{ mt: 2, mb: 4}}>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+            sx={{
+              backgroundColor: alpha('#223B7C', 1),
+              color: 'white',
+            }}
+          >
+            <Typography>{t('eligibility')}</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Grid2 size={12}>
+              <Typography component={'div'} sx={{ color: '#7f7f7f' }}>
+                {t('create_position.education_level')}
+              </Typography>
+              <RenderReadonlyTextField value={position.educationLevel} />
+            </Grid2>
+            <Grid2 size={12}>
+              <Typography component={'div'} sx={{ color: '#7f7f7f' }}>
+                {t('create_position.work_experience')}
+              </Typography>
+              <RenderReadonlyTextField value={position.workExperience} />
+            </Grid2>
+          </AccordionDetails>
+        </Accordion>
+
+        <Accordion sx={{ mt: 2, mb: 4}}>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+            sx={{
+              backgroundColor: alpha('#223B7C', 1),
+              color: 'white',
+            }}
+          >
+            <Typography>{t('additional_details')}</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <RenderReadonlyTextField value={position.details} />
+          </AccordionDetails>
+        </Accordion>
+      </Box>
+    </Box>
   );
 };
 
