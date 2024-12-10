3b154384632f50df48149878844c9c9545c648f7
Niina <170012548+niinakos@users.noreply.github.com>
2024-12-10 18:20:42 +0200

    Add employee to change log

--- backend/Virkarekisteri/src/Functions/Employees/AddPositionEmployee.cs
+++ backend/Virkarekisteri/src/Functions/Employees/AddPositionEmployee.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Components.Forms;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
@@ -51,8 +52,8 @@ public class AddPositionEmployee(
         if (position == null)
             return new OkObjectResult(createdPositionEmployee);
 
-        if (position.PricingId != null && position.PricingId.Length > 10)
-            return new BadRequestObjectResult("PricingId cannot be more than 10 characters.");
+        if (position.PricingId != null && position.PricingId.Length > 20)
+            return new BadRequestObjectResult("PricingId cannot be more than 20 characters.");
 
         if (requestPosition.Replacement)
         {
@@ -71,7 +72,21 @@ public class AddPositionEmployee(
             );
         }
         else
+        {
+            var editor = req.HttpContext.Items["Editor"] as string ?? "Unknown";
             position.PositionEmployeeId = createdPositionEmployee.Id;
+            await changeLogRepository.AddChangeLogEntry(
+                new ChangeLog
+                {
+                    PositionId = createdPositionEmployee.PositionId,
+                    EditedField = "CreatedEmployee",
+                    OldValue = string.Empty,
+                    NewValue = createdPositionEmployee.EmployeeName,
+                    Editor = editor,
+                    DecisionNumber = decisionNumber ?? "Unknown",
+                }
+            );
+        }
 
         await positionRepository.UpdatePosition(position);
 
