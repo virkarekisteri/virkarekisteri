64e4c6a8497385836007e30300ee1909bb67debf
Lassi Lappalainen <lassi.lappalainen@tuni.fi>
2025-02-07 03:20:01 +0200

    77: Bypass authentication for local dev
    * Change role authorization on backend to allow local.settings.json auth
      skip
    * Similar change on UI side to check for dev and local settings
    * Allows developers to use admin rights
    * Still requires log in to AD -- need to change this

--- backend/Virkarekisteri/src/Middleware/RoleAuthorizationMiddleware.cs
+++ backend/Virkarekisteri/src/Middleware/RoleAuthorizationMiddleware.cs
@@ -19,6 +19,15 @@ public class RoleAuthorizationMiddleware : IFunctionsWorkerMiddleware
         if (httpContext is null)
             return next(context);
 
+        // Check if the environment is local dev
+        var skipAuth = Environment.GetEnvironmentVariable("SKIP_AUTH");
+        if (skipAuth != null && skipAuth.Equals("true", StringComparison.OrdinalIgnoreCase))
+        {
+            // Bypass role authentication
+            httpContext.Items["Editor"] = "LocalDevAdmin";
+            return next(context);
+        }
+
         // Use reflection to determine if this function has the RequiredRole attribute
         // Seems like a hack, but it's the only way to do it until the ASP.NET Core middleware pipeline
         // has been implemented in the Azure Functions ASP.NET Core integration
--- frontend/src/components/role-guards.tsx
+++ frontend/src/components/role-guards.tsx
@@ -4,11 +4,17 @@ import type { PropsWithChildren } from 'react';
 import type React from 'react';
 
 export const RequiresEditRole: React.FC<PropsWithChildren> = ({ children }) => {
+  if (import.meta.env.VITE_SKIP_AUTH === 'true' && import.meta.env.DEV) {
+    return <>{children}</>;
+  }
   const isEditor = useAppSelector(selectIsEditor);
   return isEditor ? <>{children}</> : null;
 };
 
 export const RequiresAdminRole: React.FC<PropsWithChildren> = ({ children }) => {
+  if (import.meta.env.VITE_SKIP_AUTH === 'true' && import.meta.env.DEV) {
+    return <>{children}</>;
+  }
   const isAdmin = useAppSelector(selectIsAdmin);
   return isAdmin ? <>{children}</> : null;
 };
