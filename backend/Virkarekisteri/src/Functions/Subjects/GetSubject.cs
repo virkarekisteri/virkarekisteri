51e9b52a5c3e478b4198200774540a49fdcb84de
Janne Ruohoniemi <janne.ruohoniemi@tuni.fi>
2025-02-14 22:03:12 +0200

    (#93) Add APIs for handling subjects

---
+++ backend/Virkarekisteri/src/Functions/Subjects/GetSubject.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using Virkarekisteri.Middleware.Attributes;
+using Virkarekisteri.Repositories;
+
+namespace Virkarekisteri.Functions.Subjects;
+
+public class GetPosition(ILogger<GetPosition> logger, ISubjectRepository subjectRepository)
+{
+    /// <summary>
+    /// Gets a subject by id
+    /// </summary>
+    /// <param name="req">Input GET request containing the ID</param>
+    /// <returns>
+    /// The subject with the given ID
+    /// </returns>
+    [Function("GetSubject")]
+    [RequiresReadRole]
+    public async Task<IActionResult> Run(
+        [HttpTrigger(AuthorizationLevel.Function, "GET", Route = "subjects/{id}")] HttpRequest req
+    )
+    {
+        logger.LogInformation("Getting subject by id: {Id}", req.RouteValues["id"]);
+
+        if (!Guid.TryParse(req.RouteValues["id"] as string, out var subjectId))
+            return new BadRequestObjectResult($"Failed to parse {req.RouteValues["id"]} as a Guid");
+
+        var subject = await subjectRepository.GetSubject(subjectId);
+
+        if (subject is null)
+            return new NotFoundResult();
+
+        return new OkObjectResult(subject);
+    }
+}
---
+++ backend/Virkarekisteri/src/Functions/Subjects/GetSubjects.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using Virkarekisteri.Middleware.Attributes;
+using Virkarekisteri.Repositories;
+
+namespace Virkarekisteri.Functions.Subjects;
+
+public class GetSubjects(ILogger<GetSubjects> logger, ISubjectRepository subjectRepository)
+{
+    /// <summary>
+    /// Gets all subjects from the database
+    /// </summary>
+    /// <param name="req">Input GET request</param>
+    /// <returns>
+    /// All subjects in the database
+    /// </returns>
+    [Function("GetSubjects")]
+    [RequiresReadRole]
+    public async Task<IActionResult> Run(
+        [HttpTrigger(AuthorizationLevel.Function, "GET", Route = "subjects")] HttpRequest req
+    )
+    {
+        logger.LogInformation("Getting all subjects");
+        var subjects = await subjectRepository.GetSubjects();
+        return new OkObjectResult(subjects);
+    }
+}
