6f9989854ce308321eba3fca1b700d7f52963b55
Janne Ruohoniemi <janne.ruohoniemi@tuni.fi>
2025-02-26 19:41:46 +0200

    (#94) Add many-to-many relationship to DbContext

--- backend/Virkarekisteri/src/Models/VirkarekisteriDb.cs
+++ backend/Virkarekisteri/src/Models/VirkarekisteriDb.cs
@@ -10,4 +10,25 @@ public class VirkarekisteriDb(DbContextOptions<VirkarekisteriDb> options) : DbCo
     public DbSet<PositionEmployee> PositionEmployees { get; set; }
     public DbSet<ChangeLog> ChangeLogs { get; set; }
     public DbSet<Subject> Subjects { get; set; }
+    public DbSet<PositionSubject> PositionSubjects { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Many-to-many relationship between Position and Subject
+        modelBuilder.Entity<PositionSubject>().HasKey(ps => new { ps.PositionId, ps.SubjectId });
+
+        modelBuilder
+            .Entity<PositionSubject>()
+            .HasOne(ps => ps.Position)
+            .WithMany(p => p.PositionSubjects)
+            .HasForeignKey(ps => ps.PositionId)
+            .HasConstraintName("FK_PositionSubject_Position");
+
+        modelBuilder
+            .Entity<PositionSubject>()
+            .HasOne(ps => ps.Subject)
+            .WithMany(s => s.PositionSubjects)
+            .HasForeignKey(ps => ps.SubjectId)
+            .HasConstraintName("FK_PositionSubject_Subject");
+    }
 }
