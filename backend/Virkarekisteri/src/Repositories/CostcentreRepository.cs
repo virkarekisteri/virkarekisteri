56c531ed586622ee679339160ccb6ff128016a31
Janne Ruohoniemi <janne.ruohoniemi@tuni.fi>
2025-03-21 13:19:29 +0200

    (#127) Modify repositories for Costcentres

---
+++ backend/Virkarekisteri/src/Repositories/CostcentreRepository.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Virkarekisteri.Models;
+
+namespace Virkarekisteri.Repositories;
+
+public interface ICostcentreRepository
+{
+    Task<List<Costcentre>> GetAllCostcentres();
+    Task<string?> GetCostcentreNameById(Guid id);
+}
+
+public class CostcentreRepository(VirkarekisteriDb db) : ICostcentreRepository
+{
+    public async Task<List<Costcentre>> GetAllCostcentres()
+    {
+        return await db.Costcentres.ToListAsync();
+    }
+
+    public async Task<string?> GetCostcentreNameById(Guid id)
+    {
+        var costcentre = await db.Costcentres.FirstOrDefaultAsync(c => c.Id == id);
+        return costcentre?.Name;
+    }
+}
