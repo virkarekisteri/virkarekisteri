c27cad96cc329a67f70148ab6aa1d8185cd71baa
scpeky <peetu.kyllonen@tuni.fi>
2025-04-06 14:25:05 +0300

    (#133) Costcentre creation

--- frontend/src/redux/api-slices/functions/costcentre-api.ts
+++ frontend/src/redux/api-slices/functions/costcentre-api.ts
@@ -10,7 +10,26 @@ const costcentreTreesApi = baseApi.injectEndpoints({
           ? [...result.map(({ id }) => ({ type: 'CostCenters', id }) as const), { type: 'CostCenters', id: 'LIST' }]
           : [{ type: 'CostCenters', id: 'LIST' }],
     }),
+    createCostCentre: build.mutation<Costcentre, Partial<Costcentre>>({
+      query: (body) => ({
+        url: '/costcentres',
+        method: 'POST',
+        body,
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      }),
+      invalidatesTags: [{ type: 'CostCenters', id: 'LIST' }],
+    }),
+    updateCostCentre: build.mutation<Costcentre, Partial<Costcentre>>({
+      query: (body) => ({
+        url: `/costcentres/${body.id}`,
+        method: 'PUT',
+        body,
+      }),
+      invalidatesTags: (_result, _error, { id }) => [{ type: 'CostCenters', id }],
+    }),
   }),
 });
 
-export const { useGetCostCentersQuery } = costcentreTreesApi;
+export const { useGetCostCentersQuery, useCreateCostCentreMutation, useUpdateCostCentreMutation } = costcentreTreesApi;
