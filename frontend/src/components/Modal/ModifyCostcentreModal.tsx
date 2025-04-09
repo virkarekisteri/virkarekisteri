4f2e9a1d2c3cd26ee839b88b4cfa52c9cfa9d7fd
scpeky <peetu.kyllonen@tuni.fi>
2025-04-09 15:30:03 +0300

    (#133) ESLint error fix

--- frontend/src/components/Modal/ModifyCostcentreModal.tsx
+++ frontend/src/components/Modal/ModifyCostcentreModal.tsx
@@ -47,7 +47,7 @@ const EditCostcentreModal: React.FC<EditCostcentreModalProps> = ({ open, onClose
     validUntil: formatDateForInput(costcentre.validUntil),
   };
 
-  const onSubmit = async (values: any) => {
+  const onSubmit = async (values: FormValues) => {
     try {
       const updatedData = {
         number: parseInt(values.number),
