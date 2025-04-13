eefe1c0088e0088da479066abd3543551b805bc7
tupito <39724233+tupito@users.noreply.github.com>
2025-04-13 17:21:34 +0300

    Task/154 update default position names (#167)
    * Add official positionnames in the db insert script to be runned manually

--- database/scripts/PostDeployment.sql
+++ database/scripts/PostDeployment.sql
@@ -1,12 +1,12 @@
 -- This file references to SQL statements that will be executed after the build script.
 
 -- Insert data to PositionNames table
-PRINT 'INSERTING DATA TO PositionNames TABLE';
-:r .\Inserts\PositionNames.sql
+-- PRINT 'INSERTING DATA TO PositionNames TABLE';
+-- :r .\Inserts\PositionNames.sql
 
 -- Insert data to Subjects table
-PRINT 'INSERTING DATA TO Subjects TABLE';
-:r .\Inserts\Subjects.sql
+-- PRINT 'INSERTING DATA TO Subjects TABLE';
+-- :r .\Inserts\Subjects.sql
 
 -- Insert test data to Costcentres table
 --PRINT 'INSERTING DATA TO Costcentres TABLE';
