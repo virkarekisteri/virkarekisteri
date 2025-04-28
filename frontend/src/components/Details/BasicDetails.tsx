f675d51544a519681ddf23e33bccad991fb634e4
Peter Schulman <peter.schulman@tuni.fi>
2025-04-28 20:24:45 +0300

    (#186) Fix Update of data in Basic details view

--- frontend/src/components/Details/BasicDetails.tsx
+++ frontend/src/components/Details/BasicDetails.tsx
@@ -281,13 +281,13 @@ const BasicDetails: React.FC<BasicDetailsProps> = ({ position }) => {
               <Typography component={'div'} sx={{ color: '#7f7f7f' }}>
                 {t('create_position.education_level')}
               </Typography>
-              <RenderReadonlyTextField value={position.educationLevel} />
+              <RenderReadonlyTextField value={position.educationLevel || ''} /> 
             </Grid2>
             <Grid2 size={12}>
               <Typography component={'div'} sx={{ color: '#7f7f7f' }}>
                 {t('create_position.work_experience')}
               </Typography>
-              <RenderReadonlyTextField value={position.workExperience} />
+              <RenderReadonlyTextField value={position.workExperience || ''} />
             </Grid2>
           </AccordionDetails>
         </Accordion>
